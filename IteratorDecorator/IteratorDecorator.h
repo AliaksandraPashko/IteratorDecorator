7ae940127f598668da7c939bcea38000ae48f528
pashko <aliaksandra.pashko@solarwinds.com>
2018-07-12 17:10:39 +0300

    add operators a++, ++a, =

--- IteratorDecorator/IteratorDecorator.h
+++ IteratorDecorator/IteratorDecorator.h
@@ -1,54 +1,71 @@
-
 template<typename FilterFunc, typename IteratorType>
 class IteratorDecorator
 {
-	FilterFunc filter_;
-	IteratorType iterator_;
-public:
-	IteratorDecorator(FilterFunc filter, IteratorType iterator)
-		: filter_(filter), iterator_(iterator)
+	FilterFunc filter_ = 0;
+	IteratorType current_ = nullptr;
+	IteratorType end_ = nullptr;
+
+	void next_element()
 	{
-		if (!filter_(*iterator_))
+		do
 		{
-			while (!filter_(*this->iterator_))
-				iterator_++;
+			current_++;
+		} while (current_ != end_ && !filter_(*current_));
+	}
+
+public:
+
+	IteratorDecorator(FilterFunc filter, IteratorType begin, IteratorType end)
+		: filter_(filter), current_(begin), end_(end)
+	{
+		if (current_ != end_)
+		{
+			next_element();
 		}
 	}
 
 	IteratorDecorator(const IteratorDecorator &obj)
-	{
-		filter_ = obj.filter_;
-		iterator_ = obj.iterator_;
-	}
-/*
-	IteratorDecorator operator ++(IteratorDecorator& previous_it)
-	{
-		
-		IteratorDecorator new_it(this->filter_, this->iterator_);
-		while (!filter_(*this->iterator_) && )
-			previous_it->iterator_++;	
-			
-	}
-	*/
+		: filter_(obj.filter_), current_(obj.current_), end_(obj.end_) {}
 
-	decltype(*iterator_) operator * ()
+	IteratorDecorator& operator ++()
+	{	
+		next_element();
+		return *this;
+	}
+
+	IteratorDecorator operator ++(int)
 	{
-		return *iterator_;
+		IteratorDecorator it(*this);
+		next_element();
+		return it;
+	}
+
+	decltype(*current_) operator * ()
+	{
+		return *current_;
 	}
 
 	IteratorType operator -> ()
 	{
-		return iterator_;
+		return current_;
 	}
 
 	bool operator == (const IteratorDecorator &compare_it) const
 	{
-		return this->iterator_ == compare_it.iterator_;
+		return current_ == compare_it.current_;
 	}
 
 	bool operator != (const IteratorDecorator &compare_it) const
 	{
-		return this->iterator_ != compare_it.iterator_;
+		return current_ != compare_it.current_;
+	}
+
+	IteratorDecorator& operator = (const IteratorDecorator &assign_it )
+	{
+		this->filter_ = assign_it.filter_;
+		this->current_ = assign_it.current_;
+		this->end_ = assign_it.end_;
+		return *this;
 	}
 
 	~IteratorDecorator() {}
--- IteratorDecorator/Source.cpp
+++ IteratorDecorator/Source.cpp
@@ -17,9 +17,8 @@ int main()
 {
 	std::vector<int> vec(5);
 	std::iota(vec.begin(), vec.end(), 1);
-	IteratorDecorator<bool(*)(int), decltype(vec.begin())> it1(filter1, vec.begin());
-	IteratorDecorator<bool(*)(int), decltype(vec.begin())> it2(filter2, vec.begin());
-
+	IteratorDecorator<bool(*)(int), decltype(vec.begin())> it1(filter1, vec.begin(), vec.end());
+	IteratorDecorator<bool(*)(int), decltype(vec.begin())> it2(filter2, vec.begin(), vec.end());
 
 	return 0;
 }
